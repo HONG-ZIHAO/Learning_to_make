3cdb8c8a5a525bf29c2b908120b01c7684565f98
秋风三共 <h1183361884@gmail.com>
2021-05-06 18:45:11 +0900

    设置了墙壁的适配

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class RoomGenerator : MonoBehaviour
 {
-    public enum Direction { up,down,left,right};
+    public enum Direction { up, down, left, right };
     public Direction direction;
     [Header("房间信息")]
     public GameObject roomPrefab;
@@ -29,7 +29,7 @@ public class RoomGenerator : MonoBehaviour
     //记录只有一个门的房间的列表
     List<GameObject> oneWayRooms = new List<GameObject>();
 
-    // Start is called before the first frame update
+    public WallType WallType;
     void Start()
     {
         for (int i = 0; i < roomNumber; i++)
@@ -39,25 +39,18 @@ public class RoomGenerator : MonoBehaviour
             ChangePointPos();
         }
         rooms[0].GetComponent<SpriteRenderer>().color = startColor;
-        
+
         endRoom = rooms[0].gameObject;
         //找到最后的房间
         foreach (var room in rooms)
         {
-            //if (room.transform.position.sqrMagnitude>endRoom.transform.position.sqrMagnitude)
-            //{
-            //    endRoom = room.gameObject;
-            //}
+
             SetupRoom(room, room.transform.position);
         }
         FindEndRoom();
-        //if (endRoom == null)
-        //{
-        //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        //    Debug.Log("111");
-        //}
+
         endRoom.GetComponent<SpriteRenderer>().color = endColor;
-        //Debug.Log(endRoom.name);
+
     }
 
     // Update is called once per frame
@@ -89,15 +82,58 @@ public class RoomGenerator : MonoBehaviour
                     generatorPoint.position += new Vector3(xOffset, 0, 0);
                     break;
             }
-        } while (Physics2D.OverlapCircle(generatorPoint.position,0.2f,roomLayer));
+        } while (Physics2D.OverlapCircle(generatorPoint.position, 0.2f, roomLayer));
     }
-    public void SetupRoom(Room newRoom,Vector3 roomPosition)
+    public void SetupRoom(Room newRoom, Vector3 roomPosition)
     {
         newRoom.roomUp = Physics2D.OverlapCircle(roomPosition + new Vector3(0, yOffset, 0), 0.2f, roomLayer);
         newRoom.roomDown = Physics2D.OverlapCircle(roomPosition + new Vector3(0, -yOffset, 0), 0.2f, roomLayer);
         newRoom.roomLeft = Physics2D.OverlapCircle(roomPosition + new Vector3(-xOffset, 0, 0), 0.2f, roomLayer);
         newRoom.roomRight = Physics2D.OverlapCircle(roomPosition + new Vector3(xOffset, 0, 0), 0.2f, roomLayer);
         newRoom.UpdateRoom();
+        //通过开口数量和开口方向判断使用何种墙壁
+        switch (newRoom.doorNumber)
+        {
+            case 1:
+                if (newRoom.roomUp)
+                    Instantiate(WallType.singleUp, roomPosition, Quaternion.identity);
+                if (newRoom.roomDown)
+                    Instantiate(WallType.singleBottom, roomPosition, Quaternion.identity);
+                if (newRoom.roomLeft)
+                    Instantiate(WallType.singleLeft, roomPosition, Quaternion.identity);
+                if (newRoom.roomRight)
+                    Instantiate(WallType.singleRight, roomPosition, Quaternion.identity);
+                break;
+            case 2:
+                if (newRoom.roomUp && newRoom.roomDown)
+                    Instantiate(WallType.doubleUB, roomPosition, Quaternion.identity);
+                if (newRoom.roomUp && newRoom.roomLeft)
+                    Instantiate(WallType.doubleLU, roomPosition, Quaternion.identity);
+                if (newRoom.roomUp && newRoom.roomRight)
+                    Instantiate(WallType.doubleUR, roomPosition, Quaternion.identity);
+                if (newRoom.roomDown && newRoom.roomLeft)
+                    Instantiate(WallType.doubleLB, roomPosition, Quaternion.identity);
+                if (newRoom.roomDown && newRoom.roomRight)
+                    Instantiate(WallType.doubleRB, roomPosition, Quaternion.identity);
+                if (newRoom.roomLeft && newRoom.roomRight)
+                    Instantiate(WallType.doubleLR, roomPosition, Quaternion.identity);
+                break;
+            case 3:
+                if (newRoom.roomUp && newRoom.roomDown && newRoom.roomLeft)
+                    Instantiate(WallType.tripleLUB, roomPosition, Quaternion.identity);
+                if (newRoom.roomUp && newRoom.roomDown && newRoom.roomRight)
+                    Instantiate(WallType.tripleURB, roomPosition, Quaternion.identity);
+                if (newRoom.roomDown && newRoom.roomLeft && newRoom.roomRight)
+                    Instantiate(WallType.tripleLRB, roomPosition, Quaternion.identity);
+                if (newRoom.roomUp && newRoom.roomLeft && newRoom.roomRight)
+                    Instantiate(WallType.tripleLUR, roomPosition, Quaternion.identity);
+                break;
+            case 4:
+                if (newRoom.roomUp && newRoom.roomDown && newRoom.roomLeft && newRoom.roomRight)
+                    Instantiate(WallType.fourDoors, roomPosition, Quaternion.identity);
+                break;
+
+        }
     }
     public void FindEndRoom()
     {
@@ -106,7 +142,7 @@ public class RoomGenerator : MonoBehaviour
         {
             if (rooms[i].stepToStart > maxStep)
                 maxStep = rooms[i].stepToStart;
-            
+
         }
         //获得最大房间和第二大的房间
         foreach (var room in rooms)
@@ -130,12 +166,27 @@ public class RoomGenerator : MonoBehaviour
         if (oneWayRooms.Count != 0)
         {
             endRoom = oneWayRooms[Random.Range(0, oneWayRooms.Count)];
-            
+
         }
         else
         {
             endRoom = farRooms[Random.Range(0, farRooms.Count)];
-            
+
         }
     }
+}
+
+//通过WallType类来收集所有的墙壁类型，并且通过System.Serializable来将类在unity中显示出来
+[System.Serializable]
+public class WallType
+{
+    public GameObject
+        singleLeft, singleRight, singleUp, singleBottom,
+
+        doubleLU, doubleLR, doubleLB, doubleUR, doubleUB, doubleRB,
+
+        tripleLUR, tripleLUB, tripleURB, tripleLRB,
+
+        fourDoors;
+
 }
